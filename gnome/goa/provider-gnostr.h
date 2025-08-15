294f2018d6db16080f977021d8f58c3edadb0e8a
Biz <chebizarro@gmail.com>
2025-08-15 12:14:28 -0700

    fix: improve goa backend header inclusion with fallback options

--- gnome/goa/provider-gnostr.h
+++ gnome/goa/provider-gnostr.h
@@ -3,7 +3,17 @@
 #define GOA_API_IS_SUBJECT_TO_CHANGE 1
 #endif
 #include <goa/goa.h>
-#include <goa/goa-backend.h>
+#if defined(__has_include)
+#  if __has_include(<goa/goa-backend.h>)
+#    include <goa/goa-backend.h>
+#  elif __has_include(<goa/goabackend.h>)
+#    include <goa/goabackend.h>
+#  else
+     /* Fallback: some distros expose backend symbols via goa/goa.h when using goa-backend-1.0 pkg */
+#  endif
+#else
+#  include <goa/goa-backend.h>
+#endif
 #include <glib-object.h>
 
 G_BEGIN_DECLS
