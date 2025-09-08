5d64e4dd5012b22864a645b4f4898cd8e7a4f967
Biz <chebizarro@gmail.com>
2025-09-07 18:30:39 -0700

    fix: suppress pedantic compiler warnings in GLib boxed type definitions

--- libnostr/src/nostr_event_boxed.c
+++ libnostr/src/nostr_event_boxed.c
@@ -3,10 +3,17 @@
 #include <glib-object.h>
 
 /* Provide the GBoxed registration for NostrEvent using GLib-typed wrappers
- * to avoid pedantic complaints about union conversion in function pointer casts. */
+ * with exact typedef signatures to match GBoxedCopyFunc/GBoxedFreeFunc. */
 static gpointer nostr_event_copy_boxed(gconstpointer e) { return nostr_event_copy((const NostrEvent*)e); }
 static void nostr_event_free_boxed(gpointer e) { nostr_event_free((NostrEvent*)e); }
 
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 G_DEFINE_BOXED_TYPE(NostrEvent, nostr_event, nostr_event_copy_boxed, nostr_event_free_boxed)
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 #endif /* NOSTR_HAVE_GLIB */
--- libnostr/src/nostr_filter_boxed.c
+++ libnostr/src/nostr_filter_boxed.c
@@ -3,10 +3,17 @@
 #include <glib-object.h>
 #include "nostr-filter.h"
 
-/* GLib-typed wrappers to satisfy pedantic compilers */
-static gpointer nostr_filter_copy_boxed(gconstpointer f) { return nostr_filter_copy((const NostrFilter*)f); }
+/* GLib-typed wrappers matching GBoxedCopyFunc/GBoxedFreeFunc (gpointer). */
+static gpointer nostr_filter_copy_boxed(gpointer f) { return nostr_filter_copy((const NostrFilter*)f); }
 static void nostr_filter_free_boxed(gpointer f) { nostr_filter_free((NostrFilter*)f); }
 
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 G_DEFINE_BOXED_TYPE(NostrFilter, nostr_filter, nostr_filter_copy_boxed, nostr_filter_free_boxed)
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 #endif /* NOSTR_HAVE_GLIB */
