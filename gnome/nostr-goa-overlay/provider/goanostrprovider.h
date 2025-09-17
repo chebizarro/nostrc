8d6b34f272a85003055f7ab1779e1b7a303c257e
Biz <chebizarro@gmail.com>
2025-09-16 17:37:22 -0700

    feat: add GoaNostrProviderClass type definition to provider header

--- gnome/nostr-goa-overlay/provider/goanostrprovider.h
+++ gnome/nostr-goa-overlay/provider/goanostrprovider.h
@@ -6,6 +6,7 @@
 G_BEGIN_DECLS
 
 typedef struct _GoaNostrProvider GoaNostrProvider;
+typedef struct _GoaNostrProviderClass GoaNostrProviderClass;
 
 GType goa_nostr_provider_get_type(void);
 #define GOA_TYPE_NOSTR_PROVIDER (goa_nostr_provider_get_type())
