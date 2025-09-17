42530e855dd0bb1c4c9f26fabe2e3da97d9035b5
Biz <chebizarro@gmail.com>
2025-09-16 17:23:53 -0700

    refactor: simplify GoaNostrProvider type declaration and remove goabackend dependency

--- gnome/nostr-goa-overlay/provider/goanostrprovider.h
+++ gnome/nostr-goa-overlay/provider/goanostrprovider.h
@@ -2,12 +2,13 @@
 #define GOA_NOSTR_PROVIDER_H
 
 #include <glib-object.h>
-#include <goabackend/goabackend.h>
 
 G_BEGIN_DECLS
 
+typedef struct _GoaNostrProvider GoaNostrProvider;
+
+GType goa_nostr_provider_get_type(void);
 #define GOA_TYPE_NOSTR_PROVIDER (goa_nostr_provider_get_type())
-G_DECLARE_FINAL_TYPE(GoaNostrProvider, goa_nostr_provider, GOA, NOSTR_PROVIDER, GoaProvider)
 
 G_END_DECLS
 
